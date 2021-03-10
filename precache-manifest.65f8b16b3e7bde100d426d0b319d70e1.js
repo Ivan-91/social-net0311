9a1f08c82709b76a107b14831628452f92693574
Ivan-91 <jasonborn91@gmail.com>
2021-03-11 01:39:26 +0300

    Updates

---
+++ precache-manifest.65f8b16b3e7bde100d426d0b319d70e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbafd61965304fd96d3870624096fd1",
+    "url": "/social-net0311/index.html"
+  },
+  {
+    "revision": "57f4685bdf46a40d6e9a",
+    "url": "/social-net0311/static/css/main.0960a989.chunk.css"
+  },
+  {
+    "revision": "e5e074547b92854a9537",
+    "url": "/social-net0311/static/js/2.2cf6b7a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/social-net0311/static/js/2.2cf6b7a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f4685bdf46a40d6e9a",
+    "url": "/social-net0311/static/js/main.2d624d42.chunk.js"
+  },
+  {
+    "revision": "dad228587672f5dbbbaa",
+    "url": "/social-net0311/static/js/runtime-main.5a4b5963.js"
+  },
+  {
+    "revision": "667c57b7b27d000753e02b8b86c8e5b8",
+    "url": "/social-net0311/static/media/preloader.667c57b7.gif"
+  },
+  {
+    "revision": "9aafbcc9f17a9284bc999a8c6a4f767d",
+    "url": "/social-net0311/static/media/user.9aafbcc9.png"
+  }
+]);
